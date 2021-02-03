6b130c5a37a18638387ca3e5f8bdae8e105f0f90
Janviermbilizi <janviermbilizi@gmail.com>
2021-02-03 16:12:51 -0500

    require routes and install missing dependancies

--- server.js
+++ server.js
@@ -2,20 +2,21 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const expressValidator = require("express-validator");
 require("dotenv").config();
+const Routes = require("./routes");
 
 // Port that the webserver listens to
 const port = process.env.PORT || 3000;
 
 // app middleware -
 app.use(bodyParser.json());
+app.use(expressValidator());
 
 // app.use(cors()); // allows all origins
 app.use(cors({ origin: process.env.CLIENT_URL }));
 
-app.get("/", (req, res) => {
-  res.send("Welcome to The Perfect Painters Site!");
-});
+app.use("/api", Routes);
 
 app.listen(port, () => {
   console.log(`API is running on port ${port}`);
