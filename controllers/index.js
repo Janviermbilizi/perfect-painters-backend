13b3a0c47f38352997f03b787a0d0f00dc529680
Janviermbilizi <janviermbilizi@gmail.com>
2021-02-06 23:56:02 -0500

    Integrated sms notification with twilio.

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,12 @@
 const fetch = require("node-fetch");
 require("dotenv").config();
+const {
+  TWILIO_AUTH_TOKEN,
+  TWILIO_ACCOUNT_SID,
+  TWILIO_PHONE_NUMBER,
+  THE_PERFECT_PAINTERS_PHONE_NUMBER,
+} = process.env;
+const twilio = require("twilio");
 
 exports.addCustomerToGoogleSpreadsheet = (req, res) => {
   const {
@@ -30,11 +37,21 @@ exports.addCustomerToGoogleSpreadsheet = (req, res) => {
   )}&Date=${encodeURIComponent(date)}&Status=${encodeURIComponent(status)}`;
 
   fetch(url)
-    .then((sent) =>
-      res.json({
-        message: `Sent! Thank you for submitting you request, ${username}. We Will be in touch soon.`,
-      })
-    )
+    .then((sent) => {
+      const TWILIO_CLIENT = new twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
+
+      TWILIO_CLIENT.messages
+        .create({
+          body: `Hey Perfect Painters, A NEW PAINTING REQUEST JUST GOT ADDED. Name: ${username}. Phone: ${phone}. City: ${city}. Message: ${message}. For more details, please see your google spreadsheet.`,
+          to: `${THE_PERFECT_PAINTERS_PHONE_NUMBER}`, // Text this number
+          from: `${TWILIO_PHONE_NUMBER}`, // From a valid Twilio number
+        })
+        .then((sms) =>
+          res.json({
+            message: `Sent! Thank you for submitting you request, ${username}. We will be in touch soon.`,
+          })
+        );
+    })
     .catch((e) =>
       res.json({
         message: `OH No! Something went wrong, ${username}. Please try again later or call us at (301) 865-9833 to make your request.`,
